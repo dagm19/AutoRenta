b6dcf001329319771d2872aa1bb44f2ee2557e44
dagm19 <drwn03gm@gmail.com>
2025-04-28 14:10:20 -0600

    [ADD] Se crean pruebas unitarias para el modelo usuario

--- autorenta_project/usuarios/tests.py
+++ autorenta_project/usuarios/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class CustomUserTest(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username="testuser",
+            password="testpassword",
+            email="t5yKq@example.com",
+            rol="Cliente",
+        )
+        self.assertEqual(user.username, "testuser")
+        self.assertEqual(user.email, "t5yKq@example.com")
+        self.assertEqual(user.rol, "Cliente")
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        user = User.objects.create_superuser(
+            username="testsuperuser",
+            password="testpassword",
+            email="t5yKq@example.com",
+            rol="Administrador",
+        )
+        self.assertEqual(user.username, "testsuperuser")
+        self.assertEqual(user.email, "t5yKq@example.com")
+        self.assertEqual(user.rol, "Administrador")
+        self.assertTrue(user.is_active)
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
